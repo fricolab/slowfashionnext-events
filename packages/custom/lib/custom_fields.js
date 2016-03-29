e08e2ea8f70d4c43d7748384e525e8205d9eec7b
dalareo <dalareo@gmail.com>
2016-03-29 13:04:09 +0200

    added custom fields to profile

--- packages/custom/lib/custom_fields.js
+++ packages/custom/lib/custom_fields.js
@@ -32,6 +32,34 @@ Users.addField({
   }
 });
 
+Users.addField({
+  fieldName: 'customAge',
+  fieldSchema: {
+    type: String,
+    optional: true,
+    editableBy: ["member", "admin"]
+  }
+});
+
+Users.addField({
+  fieldName: 'customSex',
+  fieldSchema: {
+    type: String,
+    allowedValues: ["Mujer","Hombre","Otro"],
+    optional: true,
+    editableBy: ["member", "admin"]
+  }
+});
+
+Users.addField({
+  fieldName: 'customCountry',
+  fieldSchema: {
+    type: String,
+    optional: true,
+    editableBy: ["member", "admin"]
+  }
+});
+
 // Custom Setting Field
 
 /*Settings.addField({
